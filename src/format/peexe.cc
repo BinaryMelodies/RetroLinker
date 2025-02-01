f04e592e69e09a2a23de2249e7b2779af7c2c307
Binary Melodies <192211378+BinaryMelodies@users.noreply.github.com>
2025-02-01 19:54:35 +0100

    Preliminary input/output for PE

--- src/format/peexe.cc
+++ src/format/peexe.cc
@@ -5,15 +5,125 @@
 
 using namespace Microsoft;
 
+bool PEFormat::PEOptionalHeader::Is64Bit() const
+{
+	return magic == EXE64;
+}
+
+uint32_t PEFormat::PEOptionalHeader::GetSize()
+{
+	return (Is64Bit() ? 112 : 96) + 8 * data_directories.size();
+}
+
+void PEFormat::PEOptionalHeader::ReadFile(Linker::Reader& rd)
+{
+	magic = rd.ReadUnsigned(2);
+	version_stamp = rd.ReadUnsigned(2);
+	code_size = rd.ReadUnsigned(4);
+	data_size = rd.ReadUnsigned(4);
+	bss_size = rd.ReadUnsigned(4);
+	entry_address = rd.ReadUnsigned(4);
+	code_address = rd.ReadUnsigned(4);
+	if(!Is64Bit())
+	{
+		data_address = rd.ReadUnsigned(4);
+	}
+	image_base = rd.ReadUnsigned(Is64Bit() ? 8 : 4);
+	section_align = rd.ReadUnsigned(4);
+	file_align = rd.ReadUnsigned(4);
+	os_version.major = rd.ReadUnsigned(2);
+	os_version.minor = rd.ReadUnsigned(2);
+	image_version.major = rd.ReadUnsigned(2);
+	image_version.minor = rd.ReadUnsigned(2);
+	subsystem_version.major = rd.ReadUnsigned(2);
+	subsystem_version.minor = rd.ReadUnsigned(2);
+	win32_version = rd.ReadUnsigned(4);
+	total_image_size = rd.ReadUnsigned(4);
+	total_headers_size = rd.ReadUnsigned(4);
+	subsystem = SubsystemType(rd.ReadUnsigned(2));
+	flags = rd.ReadUnsigned(2);
+	reserved_stack_size = rd.ReadUnsigned(Is64Bit() ? 8 : 4);
+	committed_stack_size = rd.ReadUnsigned(Is64Bit() ? 8 : 4);
+	reserved_heap_size = rd.ReadUnsigned(Is64Bit() ? 8 : 4);
+	committed_heap_size = rd.ReadUnsigned(Is64Bit() ? 8 : 4);
+	loader_flags = rd.ReadUnsigned(4);
+	uint32_t directory_count = rd.ReadUnsigned(4);
+	for(uint32_t i = 0; i < directory_count; i++)
+	{
+		DataDirectory dirent;
+		dirent.address = rd.ReadUnsigned(4);
+		dirent.size = rd.ReadUnsigned(4);
+		data_directories.emplace_back(dirent);
+	}
+}
+
+void PEFormat::PEOptionalHeader::WriteFile(Linker::Writer& wr)
+{
+	wr.WriteWord(2, magic);
+	wr.WriteWord(2, version_stamp);
+	wr.WriteWord(2, code_size);
+	wr.WriteWord(2, data_size);
+	wr.WriteWord(2, bss_size);
+	wr.WriteWord(2, entry_address);
+	wr.WriteWord(2, code_address);
+	if(!Is64Bit())
+	{
+		wr.WriteWord(2, data_address);
+	}
+	wr.WriteWord(Is64Bit() ? 8 : 4, image_base);
+	wr.WriteWord(2, section_align);
+	wr.WriteWord(2, file_align);
+	wr.WriteWord(2, os_version.major);
+	wr.WriteWord(2, os_version.minor);
+	wr.WriteWord(2, image_version.major);
+	wr.WriteWord(2, image_version.minor);
+	wr.WriteWord(2, subsystem_version.major);
+	wr.WriteWord(2, subsystem_version.minor);
+	wr.WriteWord(2, win32_version);
+	wr.WriteWord(2, total_image_size);
+	wr.WriteWord(2, total_headers_size);
+	wr.WriteWord(2, subsystem);
+	wr.WriteWord(2, flags);
+	wr.WriteWord(Is64Bit() ? 8 : 4, reserved_stack_size);
+	wr.WriteWord(Is64Bit() ? 8 : 4, committed_stack_size);
+	wr.WriteWord(Is64Bit() ? 8 : 4, reserved_heap_size);
+	wr.WriteWord(Is64Bit() ? 8 : 4, committed_heap_size);
+	wr.WriteWord(2, loader_flags);
+	wr.WriteWord(2, data_directories.size());
+	for(auto& dirent : data_directories)
+	{
+		wr.WriteWord(2, dirent.address);
+		wr.WriteWord(2, dirent.size);
+	}
+}
+
+void PEFormat::PEOptionalHeader::DumpFields(COFFFormat& coff, Dumper::Dumper& dump, Dumper::Region& header_region)
+{
+	// TODO
+}
+
 void PEFormat::ReadFile(Linker::Reader& rd)
 {
-	/* TODO */
+	file_offset = rd.Tell();
+	rd.ReadData(4, pe_signature);
+	ReadCOFFHeader(rd);
+	ReadOptionalHeader(rd);
+	ReadRestOfFile(rd);
+}
+
+void PEFormat::ReadOptionalHeader(Linker::Reader& rd)
+{
+	optional_header = std::make_unique<PEOptionalHeader>();
+	optional_header->ReadFile(rd);
 }
 
 offset_t PEFormat::WriteFile(Linker::Writer& wr)
 {
-	/* TODO */
-
+	wr.endiantype = ::LittleEndian;
+	WriteStubImage(wr);
+	wr.Seek(file_offset);
+	wr.WriteData(4, pe_signature);
+	WriteFileContents(wr);
 	return offset_t(-1);
 }
 
--- src/format/peexe.h
+++ src/format/peexe.h
@@ -17,6 +17,157 @@ namespace Microsoft
 	class PEFormat : public COFF::COFFFormat, protected Microsoft::MZStubWriter
 	{
 	public:
+		char pe_signature[4];
+
+		class PEOptionalHeader : public AOutHeader
+		{
+		public:
+			static constexpr uint16_t ROM32 = 0x0107;
+			static constexpr uint16_t EXE32 = 0x010B;
+			static constexpr uint16_t EXE64 = 0x020B;
+
+			static constexpr offset_t Win32Base = 0x00400000;
+			static constexpr offset_t Dll32Base = 0x10000000;
+			static constexpr offset_t WinCEBase = 0x00010000;
+			/**
+			 * @brief Preferred base address of image, all relative virtual addresses are calculate relative to this value
+			 */
+			offset_t image_base = 0;
+			/**
+			 * @brief Section alignment
+			 */
+			uint32_t section_align = 0;
+			/**
+			 * @brief File alignment
+			 */
+			uint32_t file_align = 0;
+			/** @brief Represents a version entry with major and minor versions */
+			struct version_type
+			{
+				uint16_t major, minor;
+			};
+			/**
+			 * @brief Required operating system version
+			 */
+			version_type os_version = { };
+			/**
+			 * @brief Binary image version
+			 */
+			version_type image_version = { };
+			/**
+			 * @brief Version of the subsystem
+			 */
+			version_type subsystem_version = { };
+			/**
+			 * @brief Reserved value
+			 */
+			uint32_t win32_version = 0;
+			/**
+			 * @brief Size of the entire image, including headers
+			 */
+			uint32_t total_image_size = 0;
+			/**
+			 * @brief Cumulative size of all the headers, including the stub
+			 */
+			uint32_t total_headers_size = 0;
+			/**
+			 * @brief Checksum
+			 */
+			uint32_t checksum = 0;
+			/** Available Windows subsystems */
+			enum SubsystemType : uint16_t
+			{
+				Unknown = 0,
+				Native = 1,
+				WindowsGUI = 2,
+				WindowsCUI = 3,
+				OS2CUI = 5,
+				POSIXCUI = 7,
+				NativeWin95 = 8,
+				WinCEGUI = 9,
+				EFIApplication = 10,
+				EFIBootServiceDriver = 11,
+				EFIRuntimeDriver = 12,
+				EFIROM = 13,
+				Xbox = 14,
+				WindowsBootApplication = 16,
+			};
+			/**
+			 * @brief The Windows subsystem this program runs on
+			 */
+			SubsystemType subsystem = Unknown;
+			/**
+			 * @brief DLL flags (in PE terminology, characteristics)
+			 */
+			uint16_t flags = 0;
+			/**
+			 * @brief How much of stack should be reserved at launch
+			 */
+			offset_t reserved_stack_size = 0;
+			/**
+			 * @brief How many pages of stack are actually available at launch
+			 */
+			offset_t committed_stack_size = 0;
+			/**
+			 * @brief How much of heap should be reserved at launch
+			 */
+			offset_t reserved_heap_size = 0;
+			/**
+			 * @brief How many pages of heap are actually available at launch
+			 */
+			offset_t committed_heap_size = 0;
+			/**
+			 * @brief Reserved
+			 */
+			uint32_t loader_flags = 0;
+
+			/** @brief A data directory entry */
+			struct DataDirectory
+			{
+				uint32_t address = 0, size = 0;
+			};
+			enum
+			{
+				DirExportTable,
+				DirImportTable,
+				DirResourceTable,
+				DirExceptionTable,
+				DirCertificateTable,
+				DirBaseRelocationTable,
+				DirDebug,
+				DirArchitecture, // reserved
+				DirGlobalPointer,
+				DirTLSTable,
+				DirLoadConfigTable,
+				DirBoundImport,
+				DirIAT,
+				DirDelayImportDescriptor,
+				DirCLRRuntimeHeader,
+				DirReserved,
+				DirTotalCount,
+			};
+			/**
+			 * @brief PE specific areas in the file, each one has a specific purpose
+			 */
+			std::vector<DataDirectory> data_directories;
+
+			bool Is64Bit() const;
+
+			PEOptionalHeader()
+				: AOutHeader()
+			{
+			}
+
+			uint32_t GetSize() override;
+
+			void ReadFile(Linker::Reader& rd) override;
+
+			void WriteFile(Linker::Writer& wr) override;
+
+		protected:
+			void DumpFields(COFFFormat& coff, Dumper::Dumper& dump, Dumper::Region& header_region) override;
+		};
+
 		void ReadFile(Linker::Reader& rd) override;
 		using Linker::Format::WriteFile;
 		offset_t WriteFile(Linker::Writer& wr) override;
@@ -25,6 +176,10 @@ namespace Microsoft
 
 		using Linker::OutputFormat::GetDefaultExtension;
 		std::string GetDefaultExtension(Linker::Module& module, std::string filename) override;
+
+	protected:
+		void ReadOptionalHeader(Linker::Reader& rd);
+		using Microsoft::MZStubWriter::WriteStubImage;
 	};
 }
 
