a888949ba24758e3bc7d39c617f53825ea453528
Binary Melodies <192211378+BinaryMelodies@users.noreply.github.com>
2025-02-04 16:28:23 +0100

    Update docs

--- docs/search/enumvalues_3.js
+++ docs/search/enumvalues_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['data_0',['Data',['../classDigitalResearch_1_1CPM86Format_1_1Descriptor.html#af657b23aac9b14639cb22bf34c2847bca492c2e9bd5afcfdad00b90ca418de356',1,'DigitalResearch::CPM86Format::Descriptor']]],
-  ['data_1',['DATA',['../classCOFF_1_1COFFFormat_1_1Section.html#aa98e4f30e87e423b60d4f9d924473ff1ac312875fa4baa17c5155c3e9a387ba62',1,'COFF::COFFFormat::Section']]],
+  ['data_0',['DATA',['../classCOFF_1_1COFFFormat_1_1Section.html#aa98e4f30e87e423b60d4f9d924473ff1ac312875fa4baa17c5155c3e9a387ba62',1,'COFF::COFFFormat::Section']]],
+  ['data_1',['Data',['../classDigitalResearch_1_1CPM86Format_1_1Descriptor.html#af657b23aac9b14639cb22bf34c2847bca492c2e9bd5afcfdad00b90ca418de356',1,'DigitalResearch::CPM86Format::Descriptor']]],
   ['djgpp_2',['DJGPP',['../classCOFF_1_1COFFFormat.html#a205bb77ac2289ed12beb03a1a72d25acaf1971793850657a6e57b7c06efdc89b6',1,'COFF::COFFFormat']]]
 ];
