6216ed30932de452ec0c5c606dbc1a26fdc6c4ba
Binary Melodies <192211378+BinaryMelodies@users.noreply.github.com>
2025-11-27 15:24:31 +0100

    Win16API: attempts to fix

--- tests/win16api/window/main.c
+++ tests/win16api/window/main.c
@@ -3,8 +3,9 @@
 
 #define CLASS_NAME "SampleWindowClass"
 
-LRESULT CALLBACK MainWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK __attribute__((near_section)) MainWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+	asm volatile("pushw\t%ss\n\tpopw\t%ds"); // work around for -mno-callee-assume-ds-ss
 	switch(msg)
 	{
 	case WM_DESTROY:
@@ -25,7 +26,7 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
-int PASCAL WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int PASCAL __attribute__((near_section)) WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	MessageBox(0, "Hello World!", "Message", 0);
 	if(!hPrevInstance)
